06ab39bbaf2b8c78faf7263d444b30487fc1741f
uricohavy <uricohavy@gmail.com>
2025-11-20 13:47:27 -0800

    Music app

--- my-app/src/pages/HomePageUri.jsx
+++ my-app/src/pages/HomePageUri.jsx
@@ -30,22 +30,28 @@ export default function HomePage() {
 
   return (
     <div className="page-container">
-      <h1>Uri & Zohar's functional Website</h1>
+      <h1>Music Studio</h1>
        <div style={{ marginBottom: "1rem" }}>
-        <p>Welcome! Use the links below to navigate the site. Options may seem random but are all functional.</p>
-         <p>Try and solve the puzzles along the way!</p>
+        <p>Welcome! CohavyMusic is here to fit all you music needs</p>
+        <br></br>
+        <br></br>
+        <br></br>
+         <p>Explore our many interactive music instruments</p>
       </div>
       <div style={{ marginBottom: "1rem" }}>
         <Link to="/tone-test">
-          <button>Go to 400 Hz Test Tone</button>
+          <button>Play instruments</button>
         </Link>
-      </div>
+        </div>
+        <br></br>
+         <div style={{ marginBottom: "1rem" }}>
+     
        <div style={{ marginBottom: "1rem" }}>
-        <Link to="/search-page">
+        {/* <Link to="/search-page">
           <button>Go to Search Page for Jobs</button>
-        </Link>
+        </Link> */}
       </div>
-
+    </div>
     </div>
   );
 }
